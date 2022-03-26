f578da495b488907c5fe1c460e6a4b4f45a5c4b3
Simon Lawrence <90318503+SimonLawrenceGB@users.noreply.github.com>
2022-03-26 13:28:09 +0000

    Week 2, Exercise B
    Exercise complete.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import CaughtPokemon from "./CaughtPokemon";
 const App = () => {
   const abilities = ["Anticipation", "Adaptability", "Run-Away"];
   const date = new Date().toLocaleDateString();
+  const logWhenClicked = () => {
+    console.log("Pokedex Week 2");
+  };
   return (
     <div>
-      <Logo appName="Simon's Pokedex" />
+      <Logo appName="Simon's Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={abilities} />
       <CaughtPokemon date={date} />
     </div>
--- src/Logo.js
+++ src/Logo.js
@@ -1,16 +1,13 @@
 import React from "react";
 
 const Logo = (props) => {
-  const logWhenClicked = () => {
-    console.log("Pokedex Week 2");
-  };
   return (
     <header>
       <h1>Welcome to {props.appName}</h1>
       <img
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png"
         alt="#"
-        onClick = {logWhenClicked}
+        onClick = {props.handleClick}
       />
     </header>
   );
